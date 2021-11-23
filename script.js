d28831cdf65c16e95b2771b143c536ba17c4dc81
bek <qwerty@gmail.com>
2021-11-23 14:45:33 +0500

    script

---
+++ script.js
@@ -0,0 +1,13 @@
+var plus = prompt(' 5 + 5 ');
+var minus = prompt(' 2 - 2 ');
+var multiplication = prompt(' 3 * 3 ');
+var division = prompt(' 10 : 2 ');
+var remdevision = prompt(' 20 : 9 остаток ');
+
+
+
+console.log('5 + 5 = ' + ( 5 + 5 ) + ' ( вы ответили ' + plus + ')');
+console.log('2 - 2 = ' + ( 2 - 2 ) + '( вы ответили ' + minus + ')');
+console.log('3 * 3 = ' + ( 3 * 3 ) + '( вы ответили ' + multiplication +')');
+console.log('12 / 2 = ' + ( 10 / 2 ) + '( вы ответили ' + division +')');
+console.log('20 % 9 = ' + ( 20 % 9 ) + '( вы ответили ' + remdevision +')');
